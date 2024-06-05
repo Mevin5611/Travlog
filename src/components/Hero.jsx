1c946d4e6a56cbca5629920ff1040d01ef56da1b
Mevin5611 <mevinmevi88@gmail.com>
2024-06-05 12:57:45 +0530

    services page mobile responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import work1 from "../assets/img/work 1.png";
 import play from "../assets/img/play-circle.5 1.png";
-import layer from "../assets/img/layer.png";
-import frames from "../assets/img/Frame 15.png";
-import send from "../assets/img/send 1.png";
-import user from "../assets/img/add-user 1.png";
-import location from "../assets/img/location 1.png";
+
+import frames from "../assets/img/herobg.png";
+
+
 import brand1 from "../assets/img/Group.png";
 import brand2 from "../assets/img/Group 2.png";
 import brand3 from "../assets/img/Group (1).png";
@@ -16,14 +15,14 @@ import brand5 from "../assets/img/Group 3.png";
 const Hero = () => {
   return (
     <div className="flex items-center justify-center flex-col">
-      <div className="w-[1184px] h-[841px] grid grid-cols-12">
-        <div className="col-span-4 flex justify-start items-center ">
-          <div className="w-[412px] h-[577px]">
-            <div className="w-[224px] h-[56px] rounded-full flex justify-center items-center shadow-sm">
+      <div className="md:w-[1184px] md:h-[841px] md:grid md:grid-cols-12">
+        <div className="md:col-span-4 flex justify-start items-center mt-[450px] md:mt-[0px] absolute left-6 md:left-0 md:relative">
+          <div className="md:w-[412px] h-[577px] flex flex-col justify-center items-center">
+            <div className="md:w-[224px] h-[56px] rounded-full flex justify-center items-center shadow-sm">
               <p className="text-[#F85E9F] font-semibold">Explore the world!</p>
               <img className="ms-2 w-[20px] h-[20px]" src={work1} alt="" />
             </div>
-            <h1 className="text-[69px] font-[640] leading-[82px] mt-10">
+            <h1 className="text-[69px] font-[640] leading-[82px] mt-10 text-center md:text-left">
               Travel{" "}
               <span className="text-[#F85E9F]">
                 top <br /> destination
@@ -31,15 +30,15 @@ const Hero = () => {
               <br />
               of the world
             </h1>
-            <p className="text-[#19182580] mt-10">
-              We always make our customer happy by <br /> providing <br />
+            <p className="text-[#19182580] mt-10 text-center md:text-left">
+              We always make our customer happy by <br className="hidden md:block" /> providing <br />
               as many choices as possible{" "}
             </p>
-            <div className="flex mt-10">
-              <button className="w-[141px] h-[49px] bg-[#5D50C6] text-white rounded-full shadow-md">
+            <div className="flex flex-col md:flex-row mt-10">
+              <button className=" w-[300px] md:w-[141px] h-[49px] bg-[#5D50C6] text-white rounded-full shadow-md">
                 Get Started
               </button>
-              <div className="ms-2 w-[181px] h-[49px] bg-[#ffffff] text-white rounded-full flex justify-center items-center border">
+              <div className=" mt-3 md:mt-0 md:ms-2 w-[300px] md:w-[141px] h-[49px] bg-[#ffffff] text-white rounded-full flex justify-center items-center border">
                 {" "}
                 <img className="h-[24px] w-[24px]" src={play} alt="" />
                 <span className="text-black font-semibold ms-2">
@@ -49,39 +48,22 @@ const Hero = () => {
             </div>
           </div>
         </div>
-        <div class="col-span-8 flex   flex-col relative">
-          <div className="absolute inset-0 flex  items-center  flex-col">
-            <img className="mt-[64px] absolute inset-0" src={layer} alt="" />
-
-            <img
-              className="w-[575px] h-[632px] z-10 relative mt-[140px]"
-              src={frames}
-              alt=""
-            />
-          </div>
-          
-          <div className=" mt-[400px] ms-[80px] w-[64px] h-[64px] z-20 relative bg-[#F85E9F] rounded-full flex justify-center items-center ">
-            <img src={send} alt="" />
-          </div> 
-          <div className=" mt-[90px] ms-[610px] w-[166px] h-[64px] z-20 relative bg-[#ffffff] rounded-s-full flex justify-center items-center shadow-md">
-            <img src={location} alt="" />
-            <p className="ms-2">Top Places</p>
-          </div> 
-
-          <div className=" mt-[100px] ms-[490px] w-[64px] h-[64px] z-20 relative bg-[#FF5722] rounded-full flex justify-center items-center ">
-            <img src={user} alt="" />
-          </div>
+        <div class="md:col-span-8 flex justify-center items-start m-3 md:m-0 ">
+          <img className=" md:mt-14 " src={frames} alt="" />
           
         </div>
       </div>
-      <div className="w-[1184px] h-[160px] flex justify-between items-center">
+      <div className="md:w-[1184px] h-[160px] flex flex-col md:flex-row justify-center items-center mt-[600px] md:mt-0 m-3">
             
             
-                <div><img src={brand1} alt="" /></div>
-                <div><img src={brand2} alt="" /></div>
-                <div><img src={brand3} alt="" /></div>
-                <div><img src={brand4} alt="" /></div>
-                <div><img src={brand5} alt="" /></div>
+                <div className="grid grid-cols-3  mt-5">
+                <div className="col-span-1 flex items-center justify-center"><img src={brand1} alt="" /></div>
+                <div className="col-span-1 flex items-center justify-center"><img src={brand2} alt="" /></div>
+                <div className="col-span-1 flex items-center justify-center"><img src={brand3} alt="" /></div></div>
+                <div className="grid grid-cols-2   mt-5 md:mt-0">
+                <div className="col-span-1 flex items-center justify-center "><img src={brand4} alt="" /></div>
+                <div className="col-span-1 flex items-center justify-center"><img src={brand5} alt="" /></div>
+                </div>
             
             
       </div>
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -6,20 +6,20 @@ import cloudy from '../assets/img/cloudy 1.png'
 const Services = () => {
   return (
     <div>
-      <div className="ms-[83px] w-full h-[571px] grid grid-cols-12">
-        <div className="col-span-3 flex justify-start items-center">
+      <div className="md:ms-[83px] w-full h-[571px] md:grid md:grid-cols-12">
+        <div className="col-span-3 flex justify-center md:justify-start items-center">
           <div>
-            <h1 className="tracking-widest text-[#F85E9F] font-semibold text-lg">
+            <h1 className="tracking-widest text-[#F85E9F] font-semibold text-lg text-center md:text-left">
               SERVICES
             </h1>
-            <h3 className="font-[650]  text-[38px]">
-              Our top value <br /> categories for you
+            <h3 className="font-[650]  text-[35px] md:text-[38px] text-center md:text-left m-3">
+              Our top value <br className="hidden md:block" /> categories <br className="md:hidden"/> for you
             </h3>
           </div>
         </div>
-        <div className="col-span-9 flex justify-start items-center">
-          <div className="flex">
-            <div className="w-[350px] h-[443px]  rounded-[32px] border border-[#1918251A] flex flex-col justify-center items-center">
+        <div className="col-span-9 flex justify-center md:justify-start items-center">
+          <div className="flex flex-col md:flex-row">
+            <div className="w-[350px] h-[300px] md:h-[443px] rounded-[32px] border border-[#1918251A] flex flex-col justify-center items-center">
                 <div>
                     <img className="w-[64px] h-[64px]" src={destinantion} alt="" />
                 </div>
@@ -28,7 +28,7 @@ const Services = () => {
                     <p className="text-center mt-5 text-[#19182580]">What looked like a small <br /> patch of purple grass, <br /> above five feet.</p>
                 </div>
             </div>
-            <div className="w-[350px] h-[443px]  rounded-[32px] shadow-lg flex flex-col justify-center items-center ms-5">
+            <div className="w-[350px] h-[300px] md:h-[443px]  rounded-[32px] shadow-lg flex flex-col justify-center items-center md:ms-5">
                 <div>
                     <img className="w-[64px] h-[64px]" src={booking} alt="" />
                 </div>
@@ -37,7 +37,7 @@ const Services = () => {
                     <p className="text-center mt-5 text-[#19182580]">Square, was moving <br /> across the sand in their <br />direction.</p>
                 </div>
             </div>
-            <div className="w-[350px] h-[443px]  rounded-[32px] shadow-lg flex flex-col justify-center items-center ms-5">
+            <div className="w-[350px] h-[300px] md:h-[443px]  rounded-[32px] shadow-lg flex flex-col justify-center items-center md:ms-5">
                 <div>
                     <img className="w-[64px] h-[64px]" src={cloudy} alt="" />
                 </div>
